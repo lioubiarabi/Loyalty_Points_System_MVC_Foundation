888591c671d96a928bf8e23b3d53ffa0eb806ae7
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-15 12:39:08 +0100

    create RewardTransaction class

---
+++ src/Models/RewardTransaction.php
@@ -0,0 +1,14 @@
+<?php
+
+class RewardTransaction
+{
+    public function __construct(
+        private int $id,
+        private int $userId,
+        private string $type,
+        private int $amount,
+        private string $description,
+        private int $balanceAfter,
+        private DateTime $createdAt
+    ) {}
+}

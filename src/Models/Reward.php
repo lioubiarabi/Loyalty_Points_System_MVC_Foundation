0f22cec9689e7b26b207c00db2eb88c9f0717882
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-15 12:38:56 +0100

    create Reward class

---
+++ src/Models/Reward.php
@@ -0,0 +1,14 @@
+<?php
+
+class Reward
+{
+    public function __construct(
+        private int $id,
+        private string $name,
+        private int $pointsRequired,
+        private string $description,
+        private int $stock
+    ) {}
+
+    
+}

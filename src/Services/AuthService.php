e5f505c1dea805aa44b6ce8f15614506ccf48b71
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-16 12:29:33 +0100

    logout method in AuthService to clear the session

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -6,5 +6,9 @@ class AuthService {
     public function isLogged(): bool {
         return isset($_SESSION['user_id']);
     }
-    
+
+    public function logout() {
+        $_SESSION = [];
+        session_destroy();
+    }
 }

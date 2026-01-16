ed1371db0c6e6d451be2205b1b07947df6c9a201
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-16 11:54:44 +0100

    UserModel will create new User Enitite when it finds the user by id

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -6,13 +6,22 @@ class UserModel
         private PDO $db
     ) {}
 
-    public function findUser(string $email){
+    public function findUser(string $email)
+    {
         $stmt = $this->db->prepare("SELECT * FROM users WHERE email = ?");
         $stmt->execute([$email]);
-        
-        $user = $stmt->fetch();
-        return $user ?: null;
-    }
 
-    
+        $user = $stmt->fetch();
+        if (!$user) {
+            return null;
+        }
+        return new User(
+            $user['id'],
+            $user['email'],
+            $user['name'],
+            $user['password_hash'],
+            $user['total_points'],
+            $user['createdat']
+        );
+    }
 }

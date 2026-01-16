0d0d27642be69a42d30af7c7a36a1ac758246ab0
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-16 12:49:22 +0100

    add register method in AuthService that validate the data

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -1,15 +1,31 @@
 <?php
 session_start();
 
-class AuthService {
-    
-    public function getCurrentUser(): ?string {
-        if(isset($_SESSION['user_email'])) return $_SESSION['user_email'];
+class AuthService
+{
+
+    public function __construct(
+        private UserModel $userModel
+    ) {}
+    public function getCurrentUser(): ?string
+    {
+        if (isset($_SESSION['user_email'])) return $_SESSION['user_email'];
         return null;
     }
 
-    public function logout() {
+    public function logout()
+    {
         $_SESSION = [];
         session_destroy();
+    }
+
+    public function register(string $name, string $email, string $password): bool
+    {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) return false;
+        if(strlen($password)<8) return false;
+        if ($this->userModel->findUser($email)) return false;
+
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        return $this->userModel->create($name, $email, $hash);
     }
 }

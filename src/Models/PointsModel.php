04bd3dd87f7bf4a7b0d92757379269fd498d46cd
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-19 09:58:43 +0100

    add method to update user points

--- src/Models/PointsModel.php
+++ src/Models/PointsModel.php
@@ -5,5 +5,9 @@ class PointsModel {
         private PDO $db
     ) {}
 
-    
+    public function updatePoints(User $user, int $newBalance): bool
+    {
+        $stmt = $this->db->prepare("UPDATE users SET total_points = ? WHERE id = ?");
+        return $stmt->execute([$newBalance, $user->getId()]);
+    }
 }

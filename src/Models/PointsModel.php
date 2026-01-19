5f09c00f35559876db0f8cf81bfb5a0d87d72cc9
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-19 10:01:38 +0100

    add method to get user total points

--- src/Models/PointsModel.php
+++ src/Models/PointsModel.php
@@ -5,6 +5,13 @@ class PointsModel {
         private PDO $db
     ) {}
 
+    public function getUserPoints(User $user) {
+        $stmt = $this->db->prepare("SELECT total_points FROM users where id = ?");
+        $stmt->execute([$user->getId()]);
+
+        return $stmt->fetchColumn();
+    }
+
     public function updatePoints(User $user, int $newBalance): bool
     {
         $stmt = $this->db->prepare("UPDATE users SET total_points = ? WHERE id = ?");

319900c5587f4b5aa25cc7a8d76fa63336d35fad
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-19 09:45:30 +0100

    add methods to validate the user login and register info in auth service

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -4,9 +4,8 @@ session_start();
 class AuthService
 {
 
-    public function __construct(
-        private UserModel $userModel
-    ) {}
+    public function __construct() {}
+
     public function getCurrentUser(): ?string
     {
         if (isset($_SESSION['user_email'])) return $_SESSION['user_email'];
@@ -19,27 +18,25 @@ class AuthService
         session_destroy();
     }
 
-    public function register(string $name, string $email, string $password): bool
+    public function validateRegister(string $name, string $email, string $password)
     {
+        if (!empty($name)) return false;
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return false;
         if (strlen($password) < 8) return false;
-        if ($this->userModel->findUser($email)) return false;
-
-        $hash = password_hash($password, PASSWORD_DEFAULT);
-        return $this->userModel->create($name, $email, $hash);
+        return true;
     }
 
-    public function login($email, $password)
+    public function validateLogin($email, $password, $passhash)
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return false;
         if (strlen($password) < 8) return false;
+        return true;
+    }
 
-        $user = $this->userModel->findUser($email);
 
-        if ($user && password_verify($password, $user->getPassword())) {
-            $_SESSION['user_email'] = $user->getEmail();
-            return true;
-        }
-        return false;
+    public function login($email)
+    {
+        $_SESSION['user_email'] = $email;
+        return true;
     }
 }

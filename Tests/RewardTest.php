f53c3be5b7fb70c14d34181caeae37cb2bacb122
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-19 17:17:41 +0100

    method function to redeem a reward in RewardModel

--- Tests/RewardTest.php
+++ Tests/RewardTest.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Core\Database;
+use App\Models\PointsModel;
 use App\Models\RewardModel;
+use App\Models\UserModel;
+use App\Services\AuthService;
+use App\Services\PointsService;
 
 $rewardModel = new RewardModel(Database::connect());
 
@@ -11,3 +15,19 @@ var_dump($rewardModel->getAllRewards());
 /* get reward by id */
 echo "<br>";
 var_dump($rewardModel->getRewardById(13));
+
+/* a user reedem a reward */
+$pointM = new PointsModel(Database::connect(), new PointsService);
+echo "\n";
+
+$userM = new UserModel(Database::connect(), new AuthService);
+$user = $userM->findUser('arabi@gmail.com');
+echo "<br>User update:<br>";
+var_dump($pointM->getUserPoints($user));
+
+echo "<br>";
+var_dump($rewardModel->reedem($user, $rewardModel->getRewardById(13)));
+echo "<br>Reward update:<br>";
+var_dump($rewardModel->getRewardById(13));
+echo "<br>User update:<br>";
+var_dump($pointM->getUserPoints($user));
--- src/Models/RewardModel.php
+++ src/Models/RewardModel.php
@@ -35,7 +35,7 @@ class RewardModel
 
         $reward = $stmt->fetch();
 
-        if(!$reward) return null;
+        if (!$reward) return null;
 
         return new Reward(
             $reward['id'],
@@ -49,12 +49,51 @@ class RewardModel
     public function reedem(User $user, Reward $reward)
     {
         try {
+
+            $stmt = $this->db->prepare("SELECT total_points FROM users where id = ?");
+            $stmt->execute([$user->getId()]);
+
+            $CurrentUserPoints =  $stmt->fetchColumn();
+
+            if (!$reward->isAvailable()) {
+                throw new \Exception("Reward is out of stock");
+            }
+
+
+            if ($CurrentUserPoints < $reward->getPointsRequired()) {
+                throw new \Exception("User do't have enough points");
+            }
+
             $this->db->beginTransaction();
 
+            $stmt = $this->db->prepare("UPDATE users SET total_points = total_points - ? WHERE id = ?");
+            $stmt->execute([$reward->getPointsRequired(), $user->getId()]);
 
+            if ($reward->getStock() !== -1) {
+                $stmtStock = $this->db->prepare("UPDATE rewards SET stock = stock - 1 WHERE id = ? AND stock > 0");
+                $stmtStock->execute([$reward->getId()]);
+            }
+
+            $newBalance = $CurrentUserPoints - $reward->getPointsRequired();
+            $stmtLog = $this->db->prepare("INSERT INTO points_transactions 
+                (user_id, type, amount, description, balance_after) 
+                VALUES (?, 'redeemed', ?, ?, ?)");
+            $stmtLog->execute([
+                $user->getId(),
+                -$reward->getPointsRequired(),
+                "Echange: " . $reward->getName(),
+                $newBalance
+            ]);
+
+            
+            $stmtAward = $this->db->prepare("INSERT INTO user_rewards 
+                (user_id, reward_id, status) 
+                VALUES (?, ?, 'active')");
+            $stmtAward->execute([$user->getId(), $reward->getId()]);
+            
             $this->db->commit();
             return true;
-        } catch (\Throwable $th) {
+        } catch (\Exception $th) {
             $this->db->rollBack();
             ErrorLogger::log($th);
         }

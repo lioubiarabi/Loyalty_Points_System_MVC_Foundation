55b7d9db930262528eb9897567a62a7523769ee7
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-19 10:13:33 +0100

    create method to get all user points history

--- src/Models/PointsModel.php
+++ src/Models/PointsModel.php
@@ -1,11 +1,13 @@
 <?php
 
-class PointsModel {
+class PointsModel
+{
     public function __construct(
         private PDO $db
     ) {}
 
-    public function getUserPoints(User $user) {
+    public function getUserPoints(User $user)
+    {
         $stmt = $this->db->prepare("SELECT total_points FROM users where id = ?");
         $stmt->execute([$user->getId()]);
 
@@ -16,5 +18,26 @@ class PointsModel {
     {
         $stmt = $this->db->prepare("UPDATE users SET total_points = ? WHERE id = ?");
         return $stmt->execute([$newBalance, $user->getId()]);
+    }
+
+    public function getHistory(User $user)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM points_transactions where user_id=?");
+        $stmt->execute([$user->getId()]);
+
+        $history = [];
+        foreach ($stmt->fetchAll() as $transaction) {
+            $history[$transaction['id']] = new RewardTransaction(
+                $transaction['id'],
+                $transaction['userId'],
+                $transaction['type'],
+                $transaction['amount'],
+                $transaction['description'],
+                $transaction['balanceAfter'],
+                $transaction['createdat']
+            );
+        }
+
+        return $history;
     }
 }
